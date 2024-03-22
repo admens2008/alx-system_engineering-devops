9a878246b23de42adea37898e7d1ae1d7a2fb61e
admens2008 <adjeimensah@gmail.com>
2024-03-22 08:59:13 -0700

    installing a package fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
-package {'flask':
+# A manifest to install flask v2.1.0 on puppet-agent
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
