d026b6aed789d91a64e91c7c2397183ed646fe87
admens2008 <adjeimensah@gmail.com>
2024-03-22 19:50:35 -0700

    added werkzeug v 2.11 package to the flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 #!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
+# Install specific versions of Flask and Werkzeug
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
 
