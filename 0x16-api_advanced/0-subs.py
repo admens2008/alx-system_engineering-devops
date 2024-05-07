cf15f90be8e8f3ae353b0cefc36aa8ff8d6a64e1
admens2008 <adjeimensah@gmail.com>
2024-05-07 22:22:22 +0100

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Site24x7"
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 404:
