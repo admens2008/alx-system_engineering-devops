ca2730b85badee51a09b7f464969ca4c2d148ed4
admens2008 <138693434+admens2008@users.noreply.github.com>
2024-05-07 22:17:41 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-""" Write a function that """
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ functions to return subscribers to a subreddit """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Sie24x7"  # Replace with your custom User-Agent
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    except (requests.RequestException, KeyError):
+    response = requests.get(url, headers=headers)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
