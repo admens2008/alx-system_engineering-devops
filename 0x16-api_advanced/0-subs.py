38403c32f41ae8e6ce562a4a639b4087ada3877c
admens2008 <adjeimensah@gmail.com>
2024-05-07 22:25:44 +0100

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "Site24x7"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code >= 404:
+    response = requests.get(url, headers=headers)
+    if response.status_code >= 400:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
