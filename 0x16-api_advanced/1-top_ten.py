df61d5802639248c12bb48510f6f0efc66b33b48
admens2008 <adjeimensah@gmail.com>
2024-05-08 07:31:13 +0100

    edit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Write a function that queries the Reddit API and prints the
 titles of the first 10 hot posts listed for a given subreddit."""
+import json
 import requests
 
 
@@ -13,23 +14,26 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'MyRedditClient/1.0'}
 
     # GET request to the Reddit API
-    response = requests.get(url, headers=headers,  allow_redirects=False)
+    try:
+        response = requests.get(url, headers=headers,  allow_redirects=False)
 
-    # Check if the response is successful (status code 200)
-    if response.status_code == 200:
-        # Parsed the JSON data from the response
-        data = response.json()
-        if not response.is_json or not responce.get_json():
+        # Check if the response is successful (status code 200)
+        if response.status_code == 200:
+            # Parsed the JSON data from the response
+            data = response.json()
+            if not response.is_json or not responce.get_json():
+                print("None")
+                return
+
+            # Check if the subreddit exists
+            if 'error' in data:
+                print("None")
+                return
+
+            # Retrieve and print the titles of the first 10 posts
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        else:
             print("None")
-            return
-
-        # Check if the subreddit exists
-        if 'error' in data:
-            print("None")
-            return
-
-        # Retrieve and print the titles of the first 10 posts
-        for post in data['data']['children']:
-            print(post['data']['title'])
-    else:
+    except (requests.RequestException, KeyError, json.JSONDecodeError):
         print("None")
